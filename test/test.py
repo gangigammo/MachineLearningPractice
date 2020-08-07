350a2f95478c3c5fb8c41a271f7b7e7b023c84ac
nonofefe <yoshikawa.j.ac@m.titech.ac.jp>
2020-08-07 13:52:25 +0900

    commit figures

---
+++ test/test.py
@@ -0,0 +1,17 @@
+# import numpy as np
+#
+# # a = np.arange(12).reshape(3, 4)
+# # print(a.shape)
+# # print(a)
+# #
+# # print(np.sum(a[0]))
+# # print(np.sum(a,axis=0))
+# # print(np.sum(a,axis=1))
+#
+# a = np.arange(12).reshape(3, 4)
+# b = np.arange(12).reshape(3, 4)
+# b *= 2
+# print(a)
+# print(b)
+# for x,y in zip(a,b):
+#     print(str(x) + " " +str(y))
